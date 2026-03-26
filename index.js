f5bcdc79bef81c80fc7c7a9d4142259b5186c666
Brian767 <brra7685@colorado.edu>
2026-03-26 09:13:16 -0600

    added js functionality

--- index.js
+++ index.js
@@ -3,3 +3,31 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+// Get references to DOM elements
+const inputEl = document.getElementById("input");
+const convertBtn = document.getElementById("convert");
+const lengthEl = document.getElementById("length-result");
+const volumeEl = document.getElementById("volume-result");
+const massEl = document.getElementById("mass-result");
+
+convertBtn.addEventListener("click", function() {
+    const inputValue = Number(inputEl.value);
+    if(inputValue > 0 && inputValue < 999) {
+        const metersToFeet = (inputValue * 3.281).toFixed(3);
+        const feetToMeters = (inputValue / 3.281).toFixed(3);
+        lengthEl.textContent = `${inputValue} meters = ${metersToFeet} feet | ${inputValue} feet = ${feetToMeters} meters`;
+
+        const litersToGallons = (inputValue * 0.264).toFixed(3);
+        const gallonsToLiters = (inputValue / 0.264).toFixed(3);
+        volumeEl.textContent = `${inputValue} liters = ${litersToGallons} gallons | ${inputValue} gallons = ${gallonsToLiters} liters`;
+
+        const kilosToPounds = (inputValue * 2.204).toFixed(3);
+        const poundsToKilos = (inputValue / 2.204).toFixed(3);
+        massEl.textContent = `${inputValue} kilos = ${kilosToPounds} pounds | ${inputValue} pounds = ${poundsToKilos} kilos`;
+    } else {
+        alert("Please enter a number between 1 and 999.");
+        inputEl.value = "";
+        return;
+    }
+});
